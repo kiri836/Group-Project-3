940b70f0f5f689736b08f7295ff810bdf0dbb831
Elik Lipskar <eliklipskar@gmail.com>
2025-08-04 19:41:18 -0400

    Integration

--- main.cpp
+++ main.cpp
@@ -69,6 +69,7 @@ std::vector<std::vector<std::string>> loadDictionary(std::vector<std::vector<std
 }
 
 int main() {
+    std::string option;
     std::string wordToLookup;
     std::vector<std::string> definitions;
     std::vector<std::vector<std::string>> dictionary;
@@ -80,43 +81,78 @@ int main() {
     hashTable.loadWords(dictionary);
 
     while (true) {
-        std::cout << "\nEnter a word to look up (or type 'quit' to exit): ";
-        std::getline(std::cin, wordToLookup);
+        std::cout << "\nMenu:" << std::endl;
+        std::cout << "0. Exit" << std::endl;
+        std::cout << "1. Definition generator" << std::endl;
+        std::cout << "2. Prefix matches" << std::endl;
+        std::cout << "\nChoose an option (0, 1, or 2): ";
+        getline(std::cin, option);
 
-        // Convert to matching case
-        for (char &c : wordToLookup) {
-            c = std::tolower(c);
+        if (option == "0") {
+            std::cout << "Goodbye!" << std::endl;
+            break; // Exit the loop
         }
 
-        // Safeguards against empty string if user inputs enter key
-        if (!wordToLookup.empty())
-            wordToLookup[0] = std::toupper(wordToLookup[0]);
+        else if (option == "1") {
+            std::cout << "\nEnter a word to look up (or type '1' to exit): ";
+            std::getline(std::cin, wordToLookup);
 
-        // Check if the user wants to quit.
-        if (wordToLookup == "Quit") {
-            break; // Exit the loop.
+            // Check if the user wants to quit.
+            if (wordToLookup == "1") {
+                std::cout << "Goodbye!" << std::endl;
+                break; // Exit the loop.
+            }
+
+            // Convert to matching case
+            for (char &c : wordToLookup) {
+                c = std::tolower(c);
+            }
+
+            // Safeguards against empty string if user inputs enter key
+            if (!wordToLookup.empty())
+                wordToLookup[0] = std::toupper(wordToLookup[0]);
+
+            // Use a linear search to find the word
+            auto startLinear = chrono::high_resolution_clock::now();
+            definitions = linearSearch(dictionary, wordToLookup);
+            auto endLinear = chrono::high_resolution_clock::now();
+            auto startTrie = chrono::high_resolution_clock::now();
+            std::vector<std::string> trieDefinitions = trieDictionary.search(wordToLookup);
+            auto endTrie = chrono::high_resolution_clock::now();
+            auto startTable = chrono::high_resolution_clock::now();
+            std::vector<std::string> hashTableDefinitions = hashTable.search(wordToLookup);
+            auto endTable = chrono::high_resolution_clock::now();
+
+            for (int i = 0; i < definitions.size(); i++) {
+                std::cout << i + 1 << ". " << definitions[i] << std::endl;
+            }
+
+            // Print out search times for each algorithm
+            std::cout << std::endl;
+            std::cout << "Linear search time: " << chrono::duration_cast<chrono::microseconds>(endLinear - startLinear).count() << " microseconds" << std::endl;
+            std::cout << "Trie search time: " << chrono::duration_cast<chrono::microseconds>(endTrie - startTrie).count() << " microseconds" << std::endl;
+            std::cout << "Hashtable search time: " << chrono::duration_cast<chrono::microseconds>(endTable - startTable).count() << " microseconds" << std::endl;
         }
 
-        // Use a linear search to find the word
-        auto startLinear = chrono::high_resolution_clock::now();
-        definitions = linearSearch(dictionary, wordToLookup);
-        auto endLinear = chrono::high_resolution_clock::now();
-        auto startTrie = chrono::high_resolution_clock::now();
-        std::vector<std::string> trieDefinitions = trieDictionary.search(wordToLookup);
-        auto endTrie = chrono::high_resolution_clock::now();
-        auto startTable = chrono::high_resolution_clock::now();
-        std::vector<std::string> hashTableDefinitions = hashTable.search(wordToLookup);
-        auto endTable = chrono::high_resolution_clock::now();
+        else if (option == "2") {
+            string prefix;
+            std::cout << "Enter a prefix (or '1' to exit): ";
+            std::getline(std::cin, prefix);
+            if (prefix == "1") {
+                std::cout << "Goodbye!" << std::endl;
+                break;
+            }
 
-        for (int i = 0; i < definitions.size(); i++) {
-            std::cout << i + 1 << ". " << definitions[i] << std::endl;
+            vector<string> matches = trieDictionary.prefix(prefix);
+            if (matches.size() != 0) {
+                std::cout << "Prefix matches: " << std::endl;
+                for (int i = 0; i < matches.size(); i++) {
+                    std::cout << i + 1 << ". " << matches[i] << std::endl;
+                }
+
+                std::cout << "\nYour prefix generated " << matches.size() << " matches!" << std::endl;
+            }
         }
-
-        // Print out search times for each algorithm
-        std::cout << std::endl;
-        std::cout << "Linear search time: " << chrono::duration_cast<chrono::microseconds>(endLinear - startLinear).count() << " microseconds" << std::endl;
-        std::cout << "Trie search time: " << chrono::duration_cast<chrono::microseconds>(endTrie - startTrie).count() << " microseconds" << std::endl;
-        std::cout << "Hashtable search time: " << chrono::duration_cast<chrono::microseconds>(endTable - startTable).count() << " microseconds" << std::endl;
     }
 
     return 0;
